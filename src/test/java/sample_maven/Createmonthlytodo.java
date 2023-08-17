803d870b76ccf57e286657a404cd1307f8cded68
Nani <Nani@DESKTOP-GTEVIFT>
2023-08-17 11:18:25 +0530

    new calendar todo created

---
+++ src/test/java/sample_maven/Createmonthlytodo.java
@@ -0,0 +1,11 @@
+package sample_maven;
+
+import org.testng.annotations.Test;
+
+public class Createmonthlytodo {
+	@Test 
+	public void createtodo() {
+		System.out.println("to do created ");
+	}
+
+}
