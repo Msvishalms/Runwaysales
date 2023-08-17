8d0015d3fa2175590c890a9194c5b9ab322aab22
Nani <Nani@DESKTOP-GTEVIFT>
2023-08-17 10:47:08 +0530

    created new Campaigns with all mandatory fields

---
+++ src/test/java/sample_maven/CreateCampWithMandatory.java
@@ -0,0 +1,11 @@
+package sample_maven;
+
+import org.testng.annotations.Test;
+
+public class CreateCampWithMandatory {
+	@Test 
+	public void CreateCamp() {
+		System.out.println("---------new campaign is created");
+	}
+
+}
