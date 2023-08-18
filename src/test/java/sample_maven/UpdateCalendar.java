dc18361af5f627326b67a33ef1cb55aba0009cce
Msvishalms <msvishal945@gmail.com>
2023-08-18 14:56:06 +0530

    update

---
+++ src/test/java/sample_maven/UpdateCalendar.java
@@ -0,0 +1,12 @@
+package sample_maven;
+
+import org.testng.annotations.Test;
+
+public class UpdateCalendar {
+
+	@Test
+	public void update()
+	{
+		System.out.println("----update calendar---");
+	}
+}
