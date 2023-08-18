a2650f0b5e1ebb3a23657da08fdb39445f427c15
Msvishalms <msvishal945@gmail.com>
2023-08-18 14:49:37 +0530

    k

--- src/test/java/sample_maven/Samp.java
+++ src/test/java/sample_maven/Samp.java
@@ -2,10 +2,16 @@ package sample_maven;
 
 import org.testng.annotations.Test;
 
-public class Samp {
+public class Samp 
+{
 	@Test(groups="smoke")
 	public void extra()
 	{
-		System.out.println("--extra     --");
+		System.out.println("--extra---");
+	}
+	
+	public void moreExtra()
+	{
+		System.out.println("---more--");
 	}
 }
